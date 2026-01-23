dc52682ec5522e5bcfe82e37b010fca650b5d1aa
Kletca <h3yseilend@gmail.com>
2026-01-23 19:10:03 +0200

    I am practicing working with CRUD operations.

---
+++ Library/Books.cs
@@ -0,0 +1,92 @@
+﻿namespace Library;
+
+public class Books
+{
+    // private fields
+    private string _title;
+    private string _author;
+    private int _pages;
+    private int _year;
+    private int _isbn;
+    
+    //constructor
+    public Books(string title, string author, int pages, int year, int isbn)
+    {
+        _title = title;
+        _author = author;
+        _pages = pages;
+        _year = year;
+        _isbn = isbn;
+    }
+    
+    //Properties
+    public string Title
+    {
+        get { return _title; }
+    }
+
+    public string Author
+    {
+        get { return _author; }
+    }
+
+    public int Pages
+    {
+        get { return _pages; }
+    }
+
+    public int Year
+    {
+        get { return _year; }
+    }
+
+    public int Isbn
+    {
+        get { return _isbn; }
+    }
+
+    //Create a comment
+    private string[] _comments = new string[5];
+    private int _commentCount = 0;
+    public void CreateComments(string comments)
+    {
+        _comments[_commentCount] = comments;
+        _commentCount++;
+    }
+    
+    //Read the comments
+    public void ReadComments()
+    {
+        if (_commentCount == 0)
+        {
+            Console.WriteLine("No comments found!");
+            return;
+        }
+
+        for (int i = 0; i < _commentCount; i++)
+        {
+            Console.WriteLine($"Read the comment: {_comments[i]}");
+        }
+    }
+    
+    //Updating comments
+    public void UpdateComments(int index, string comments)
+    {
+        for (int i = 0; i < _commentCount; i++)
+        {
+            if (index >= 0 && index < _comments.Length)
+            {
+                _comments[i] = comments;
+            }
+        }
+    }
+    
+    //Deleting comments
+    public void DeleteComments(int index)
+    {
+        if (index >= 0 && index < _comments.Length)
+        {
+            _comments[index] = null;
+        }
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books[] books = new Books[3];
+
+            books[0] = new Books( "Charli", "The Hobbit", 354, 1937, 978054579);
+            books[1] = new Books("Alice in the Country of Clover: Nightmare", "QuinRose (writer), Yobu (artist)", 192, 2014, 978054579);
+            books[2] = new Books("Another Book", "Some Author", 100, 2020, 978054579);
+            
+            books[0].CreateComments("I am delighted with this book");
+            books[0].CreateComments("very bad book((");
+            books[1].CreateComments("Interesting");
+            books[1].CreateComments("I don't like it");
+            books[2].CreateComments("I am like this book");
+            books[2].CreateComments("complete nonsense");
+            
+            books[0].ReadComments();
+            books[1].ReadComments();
+            books[2].ReadComments();
+        }
+    }
+}
